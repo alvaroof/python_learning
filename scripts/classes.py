86f3aff75da528e432e1fa67c03e750232933747
alvaroof <ortiz.fernandez.alvaro@gmail.com>
2023-12-28 13:09:38 +0000

    Bike Class

--- scripts/classes.py
+++ scripts/classes.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import random
+import time
 from enum import Enum
 
 
@@ -9,12 +11,19 @@ class Condition(Enum):
     BAD = 3
 
 
+class MethodNowAllowed(Exception):
+    pass
+
+
 class Bike:
+    num_wheels = 2
+    counter = 0
+
     def __init__(
         self,
-        description: str = "Really Beautiful",
-        condition: str = "Perfect Condition",
-        sale_price: float = 0,
+        description: str,
+        condition: Condition = Condition.GOOD,
+        sale_price: float = 100,
         cost: float = 0,
         year: int = 2015,
     ):
@@ -33,24 +42,29 @@ class Bike:
         """
         self.description = description
         self.condition = condition
-        self.sale_price = sale_price
+        self._sale_price = None  # Private
         self.cost = cost
+        self._slow_attribute = None
 
         self.age = 2023 - year
         self.sold = False
         self.premium = None
+        self.update_sale_price(sale_price)
+        Bike.counter += 1
 
     def update_sale_price(self, sale_price: float):
         if self.sold:
-            raise AttributeError("Cannot update sale price of a sold bike.")
+            raise MethodNowAllowed("Cannot update sale price of a sold bike.")
         if sale_price <= 0:
             raise ValueError("Sale price must be greater than zero.")
-        self.sale_price = sale_price
+        self._sale_price = sale_price
 
     def is_premium(self):
         self.premium = "Yes"
 
     def sell(self) -> float:
+        if self.sold:
+            raise MethodNowAllowed("Cannot update sale price of a sold bike.")
         self.sold = True
         profit = self.sale_price - self.cost
         print(f"Sold for a profit: {profit}")
@@ -63,9 +77,68 @@ class Bike:
         if new_condition is not None:
             self.condition = new_condition
 
+    @property
+    def sale_price(self):
+        return self._sale_price
+
+    @sale_price.setter
+    def sale_price(self, sale_price: float):
+        if self.sold:
+            raise MethodNowAllowed("Cannot update sale price of a sold bike.")
+        if sale_price <= 0:
+            raise ValueError("Sale price must be greater than zero.")
+        self._sale_price = sale_price
+
+    @property
+    def profit(self):
+        return self.sale_price - self.cost
+
+    @property  # Used when an attribute is computationally intensive to calculate. Cache version is accessible
+    def slow_attribute(self):
+        if self._slow_attribute is not None:
+            print("Slow Attribute was already cached")
+            return self._slow_attribute
+        else:
+            print("Calculating Slow Attribute")
+            time.sleep(5)
+            self._slow_attribute = "Set"
+            return self._slow_attribute
+
     @staticmethod
     def sing_the_bike_song():
         print("Singing the Bike Song")
+
+    def __add__(self, other):
+        if isinstance(other, Bike):
+            self.cost += other.cost
+
+    def __del__(self):
+        Bike.counter -= 1
+        print("Bike deleted")
+
+    def __str__(self):
+        """Called when str() or print()"""
+        return f"{self.description}: ${self.sale_price}"
+
+    def __repr__(self):
+        return f"Bike({self.description}, {self.condition}, {self.sale_price}, {self.cost})"
+
+    @staticmethod
+    def get_test_bike():
+        return Bike(condition=Condition.GOOD, sale_price=1000, cost=0, description=Bike.__name__)
+
+    @classmethod
+    def get_test_object(cls):
+        return cls(
+            condition=random.choice(list(Condition)),
+            sale_price=1000,
+            cost=0,
+            description=f"{cls.__name__}",
+        )
+
+
+class Unicycle(Bike):
+    num_wheels = 1
 
 
 if __name__ == "__main__":
@@ -76,10 +149,10 @@ if __name__ == "__main__":
         cost=100,
         year=1965,
     )
-    print(my_bike)
-    print(type(my_bike))
-    print(my_bike.condition)
-    my_bike.service(cost=100, new_condition=Condition.OKAY, new_sale_price=1200)
-    print(my_bike.condition)
-    my_bike.sell()
-    my_bike.sing_the_bike_song()
+    test_unicycle = Unicycle.get_test_object()
+    test_bike = Bike.get_test_bike()
+    print(test_unicycle)
+    print(test_bike)
+    print([test_unicycle, test_bike])
+    print(my_bike.slow_attribute)
+    print(my_bike.slow_attribute)
